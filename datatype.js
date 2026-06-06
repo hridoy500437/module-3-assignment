6a44d4a89ba40884f01f88458e97ca4da8ae5242
hridoy500437 <sakib228236@gmail.com>
2026-06-06 23:48:28 +0600

    Add data types check script with console logs

---
+++ datatype.js
@@ -0,0 +1,21 @@
+//! Task 2: Data Types Check
+
+let names = "Hridoy";
+
+console.log(typeof names);
+
+let age = 24;
+
+console.log(typeof age);
+
+let isStudent = true;
+
+console.log(typeof isStudent);
+
+let salary = null;
+
+console.log(typeof salary );
+
+let phone;
+
+console.log(typeof phone);
