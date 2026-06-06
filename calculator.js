69b21e5f975383b1508b504fccc2ceb17ea44b8c
hridoy500437 <sakib228236@gmail.com>
2026-06-06 23:50:15 +0600

    Add simple calculator implementation with basic arithmetic operations

---
+++ calculator.js
@@ -0,0 +1,25 @@
+//! Task 3: Simple Calculator
+
+let num1 = 10;
+
+let num2 = 5;
+
+let Addition = num1 + num2;
+
+console.log("Addition: ",Addition);
+
+let Subtraction = num1 - num2;
+
+console.log("Subtraction: ",Subtraction);
+
+let Multiplication = num1 * num2;
+
+console.log("Multiplication: ",Multiplication);
+
+let Division = num1 / num2;
+
+console.log("Division: ",Division);
+
+let Modulus = num1 % num2;
+
+console.log("Modulus: ",Modulus);
