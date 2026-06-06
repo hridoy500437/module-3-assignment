6ab719cb910ede9edd8d8e099ec4a94a35ce2c47
hridoy500437 <sakib228236@gmail.com>
2026-06-06 23:46:12 +0600

    Add student information script with basic details and console logs

---
+++ student.js
@@ -0,0 +1,17 @@
+//! Task 1: Student Information(Using Declare variable)
+
+let Name =  "Hridy";
+
+let Age =  24 ;
+
+let District =  "Dinajpur"
+
+let Student =  true;
+
+console.log(`My name is: ${Name}`);
+
+console.log("Age :",Age);
+
+console.log("Distric: ",District);
+
+console.log("isStudent: ",Student);
